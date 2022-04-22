751f06de3ed65955d1ff86e87d32dc64439f116c
Benito "DK_Den7e" L <benitoll1993@gmail.com>
2022-04-22 06:28:02 -0700

    Proper crouching (restores walkstyle after)

--- client/Walk.lua
+++ client/Walk.lua
@@ -1,6 +1,43 @@
+local latestWalkStyle = nil
+local crouched = false
+
+
+Citizen.CreateThread(function()
+	while true do
+		Citizen.Wait(1)
+		DisableControlAction(0,36,true)--INPUT_DUCK
+	end
+end)
+
+RegisterCommand("crouch", function()
+    playerPed = PlayerPedId()
+    if (DoesEntityExist(playerPed) and not IsEntityDead(playerPed)) then
+    	if (not IsPauseMenuActive()) then
+    		RequestAnimSet("move_ped_crouched")
+    		while (not HasAnimSetLoaded("move_ped_crouched")) do
+    			Citizen.Wait(25)
+    		end
+    		if crouched then
+    			if latestWalkStyle then
+    				SetPedMovementClipset(playerPed, latestWalkStyle, 0.2)
+    			else
+    				ResetPedMovementClipset(playerPed, 0)
+    			end
+    			crouched = false
+    		else
+    			SetPedMovementClipset(playerPed, "move_ped_crouched", 0.25)
+    			crouched = true
+    		end
+    	end
+    end
+end)
+
+RegisterKeyMapping("crouch", "Agacharse/levantarse", "keyboard", "LCONTROL")
+
 function WalkMenuStart(name)
     RequestWalking(name)
     SetPedMovementClipset(PlayerPedId(), name, 0.2)
+    latestWalkStyle = name
     RemoveAnimSet(name)
 end
 
